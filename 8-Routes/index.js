521d77bdb37972f99ebf8d683d1f855c9b070778
osckli998 <osckli998@edu.linkoping.se>
2026-09-21 17:57:27 +0000

    oij

--- 8-Routes/index.js
+++ 8-Routes/index.js
@@ -2,14 +2,26 @@ const express = require("express");
 
 const app = express();
 const port = 3000;
+app.use(morgan(`dev`));
 
 app.get("/monsters/add/list", (req, res) => {
-  res.send("Goblin, Kobold, Mother-in-Law");
+  // res.send("Goblin, Kobold, Mother-in-Law");
+  const monsters = [`Goblin`, `Kobold`, `Mother-in-Law`];
+  const sortOrder = req.query.sort;
+  let sortedMonsters = [...monsters];
+
+  if (sortOrder === `asc`) {
+    sortedMonsters.sort();
+  } else if (sortOrder === `desc`) {
+    sortedMonsters.sort().reverse();
+  }
+
+  res.send(sortedMonsters.join(`,`));
 });
 
 app.get("/monsters/add/:count", (req, res) => {
   const amount = req.params.count;
-  res.send(amount);
+  res.send(`Ok ${amount}`);
 });
 
 app.get("/name/:person", (req, res) => {
