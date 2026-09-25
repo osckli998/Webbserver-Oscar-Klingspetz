28f2fbb05ba7c7eb04966314c5a1921b35ff21b2
osckli998 <osckli998@edu.linkoping.se>
2026-09-25 08:29:09 +0000

    ad

--- 8-Routes/index.js
+++ 8-Routes/index.js
@@ -1,33 +1,34 @@
 const express = require("express");
-
+const morgan = require("morgan");
 const app = express();
 const port = 3000;
 app.use(morgan(`dev`));
 
-app.get("/monsters/add/list", (req, res) => {
-  // res.send("Goblin, Kobold, Mother-in-Law");
-  const monsters = [`Goblin`, `Kobold`, `Mother-in-Law`];
-  const sortOrder = req.query.sort;
-  let sortedMonsters = [...monsters];
+app.use(express.static(`public`));
 
-  if (sortOrder === `asc`) {
-    sortedMonsters.sort();
-  } else if (sortOrder === `desc`) {
-    sortedMonsters.sort().reverse();
-  }
+// app.get("/monsters/add/list", (req, res) => {
+//   const monsters = [`Goblin`, `Kobold`, `Mother-in-Law`];
+//   const sortOrder = req.query.sort;
+//   let sortedMonsters = [...monsters];
 
-  res.send(sortedMonsters.join(`,`));
-});
+//   if (sortOrder === `asc`) {
+//     sortedMonsters.sort();
+//   } else if (sortOrder === `desc`) {
+//     sortedMonsters.sort().reverse();
+//   }
 
-app.get("/monsters/add/:count", (req, res) => {
-  const amount = req.params.count;
-  res.send(`Ok ${amount}`);
-});
+//   res.send(sortedMonsters.join(`,`));
+// });
 
-app.get("/name/:person", (req, res) => {
-  const name = req.params.person;
-  res.send(`You be Rizzlord ${name}`);
-});
+// app.get("/monsters/add/:count", (req, res) => {
+//   const amount = req.params.count;
+//   res.send(`Ok ${amount}`);
+// });
+
+// app.get("/name/:person", (req, res) => {
+//   const name = req.params.person;
+//   res.send(`You be Rizzlord ${name}`);
+// });
 
 app.listen(port, () => {
   console.log(`Servern körs på ${port}`);
