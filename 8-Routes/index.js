40b709a4ec69389f1cf49c4be2d26a6ec0bf939d
osckli998 <osckli998@edu.linkoping.se>
2026-09-26 18:44:31 +0000

    chjnBFCHV CV sc

--- 8-Routes/index.js
+++ 8-Routes/index.js
@@ -4,31 +4,21 @@ const app = express();
 const port = 3000;
 app.use(morgan(`dev`));
 
+app.use(morgan(`dev`));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(`public`));
 
-// app.get("/monsters/add/list", (req, res) => {
-//   const monsters = [`Goblin`, `Kobold`, `Mother-in-Law`];
-//   const sortOrder = req.query.sort;
-//   let sortedMonsters = [...monsters];
+app.use(express.static(`public`));
 
-//   if (sortOrder === `asc`) {
-//     sortedMonsters.sort();
-//   } else if (sortOrder === `desc`) {
-//     sortedMonsters.sort().reverse();
-//   }
+app.get("/", (req, res) => {
+  res.send("Hej från kapitel 8!");
+});
+app.post("/hit-me", (req, res) => {
+  console.log("BLabla", req.body);
 
-//   res.send(sortedMonsters.join(`,`));
-// });
-
-// app.get("/monsters/add/:count", (req, res) => {
-//   const amount = req.params.count;
-//   res.send(`Ok ${amount}`);
-// });
-
-// app.get("/name/:person", (req, res) => {
-//   const name = req.params.person;
-//   res.send(`You be Rizzlord ${name}`);
-// });
+  res.send("Ojk");
+});
 
 app.listen(port, () => {
   console.log(`Servern körs på ${port}`);
